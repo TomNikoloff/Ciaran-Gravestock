a2956d86e742829c7a8d5ca1f25f0631979c7ba7
TomNikoloff <tomnikoloff@hotmail.co.uk>
2021-04-21 20:38:42 +0100

    fixing php handler

--- contact_form_handler.php
+++ contact_form_handler.php
@@ -4,20 +4,20 @@
     $message = $_POST['message'];
 
 
-    $email_from = 'visitor email';
+    $email_from = 'siana.9607@hotmail.co.uk';
 
-    $email_subject = "New Form Submission"
+    $email_subject = "New Form Submission";
 
     $email_body = "User Name: $name.\n".
                     "User Email: $vistor_email.\n".
                         "User Message: $message.\n";
 
     
-    $to = "Ciarans Email";
+    $to = "tomnikoloff@hotmail.co.uk";
 
     $headers = "From: $email_from \r\n";
 
-    $headers .= "Reply_To: $visitors_email \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
     mail($to,$email_subject,$email_body,$headers);
 
