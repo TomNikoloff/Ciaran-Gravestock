1743e59cf34e9cb8a86155b879021291cfc974ae
TomNikoloff <tomnikoloff@hotmail.co.uk>
2021-04-21 21:14:59 +0100

    .

--- contact_form_handler.php
+++ contact_form_handler.php
@@ -1,26 +1,28 @@
 <?php
-    $name= $_POST['name'];
-    $vistor_email = $_POST['email'];
-    $message = $_POST['message'];
 
+    if(isset($_POST['email']) && $POST['email'] != ''){
 
-    $email_from = 'siana.9607@hotmail.co.uk';
-
-    $email_subject = "New Form Submission";
-
-    $email_body = "User Name: $name.\n".
-                    "User Email: $vistor_email.\n".
-                        "User Message: $message.\n";
-
+        if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+                   
+        $userName = $_POST['name'];
+        $UserEmail = $_POST['email'];
+        $messageSubject = $_POST['subject'];
+        $message = $_POST['message'];
+        
+        $to = "ciaran@gravestock.net";
+        $body = "";
     
-    $to = "tomnikoloff@hotmail.co.uk";
+        $body .= "From: ".$userName. "\r\n";
+        $body .= "Email: ".$userEmail. "\r\n";
+        $body .= "Message: ".$message. "\r\n";
+    
+    
+        mail($to,$messageSubject,$body);
+    
+        }
 
-    $headers = "From: $email_from \r\n";
 
-    $headers .= "Reply-To: $visitor_email \r\n";
+    }
 
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: contact.html");
 
 ?>
